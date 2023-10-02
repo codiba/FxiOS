0d9adb2b9878a233084b72f72f88f100ca7357d3
Salihcan Kahya <salihcankahya@gmail.com>
2023-10-02 14:22:08 +0300

    project init

---
+++ FxiOS/FxiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FxiOSApp.swift
+//  FxiOS
+//
+//  Created by Salihcan Kahya on 2.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FxiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
